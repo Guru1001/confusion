c7ceaf3b17250acaa6720d43fc3d182615da2309
Guruprasad Tupe <prasadtupe22@gmail.com>
2020-09-25 14:10:06 +0530

    Menu component part 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes'
 
 function App() {
+  const [dishes] = useState(DISHES);
   return (
     <div>
       <Navbar dark color="primary">
@@ -10,7 +12,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={dishes}/>
     </div>
   );
 }
