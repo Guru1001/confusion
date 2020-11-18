c09e2f29a34aeeef7356dc76609b078ce603ca5d
Guruprasad Tupe <prasadtupe22@gmail.com>
2020-11-18 12:59:08 +0530

    Updates

---
+++ precache-manifest.1317dda4c2149ccc37e1e8ac037b87a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56647d1d2db418f7f53e6d061f8e158e",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "d2dd0c4cf3792b0c8962",
+    "url": "/confusion/static/css/2.f1f8a52f.chunk.css"
+  },
+  {
+    "revision": "ba9313300dabcb31d87a",
+    "url": "/confusion/static/css/main.cc19edd1.chunk.css"
+  },
+  {
+    "revision": "d2dd0c4cf3792b0c8962",
+    "url": "/confusion/static/js/2.6841748e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/confusion/static/js/2.6841748e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9313300dabcb31d87a",
+    "url": "/confusion/static/js/main.48a7235f.chunk.js"
+  },
+  {
+    "revision": "47b295cfc61f796a9c36",
+    "url": "/confusion/static/js/runtime-main.19a3f2b7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
